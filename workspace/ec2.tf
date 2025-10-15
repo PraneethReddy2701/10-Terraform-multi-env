35952a92a154940491a9fd6cab0f14c14b0ee9b5
Praneeth Reddy <praneeth.kotholla@gmail.com>
2025-10-15 11:15:49 +0530

    workspace

---
+++ workspace/ec2.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "roboshop" {
+  count                  = length(var.instances)
+  ami                    = var.ami_id #this is our daily used AMI
+  instance_type          = lookup(var.instance_type, terraform.workspace)
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.instances[count.index]}-${terraform.workspace}"
+      component = var.instances[count.index]
+      environment = terraform.workspace
+    }
+  )
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "${var.project}-${var.sg_name}-${terraform.workspace}"
+  description = var.sg_description
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.sg_name}-${terraform.workspace}"
+    }
+  )
+
+}
---
+++ workspace/variables.tf
@@ -0,0 +1,66 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "This is our daily used AMI"
+}
+
+variable "instances" {
+  default = ["mongodb", "redis", "mysql", "rabbitmq"]
+
+}
+
+variable "instance_type" {
+  default = {
+
+    dev = t3.micro
+    prod = t3.small
+  }
+  
+}
+
+variable "project" {
+  default = "roboshop"  
+}
+
+variable "sg_name" {
+  default = "allow_all"
+}
+
+variable "sg_description" {
+  default = "Allowing all ports from internet"
+}
+
+variable "from_port" {
+  type    = number
+  default = 0
+}
+
+variable "to_port" {
+  type    = number
+  default = 0
+}
+
+variable "cidr_blocks" {
+  default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+  default = {
+    Name = "allow-all"
+  }
+}
+
+variable "zone_id" {
+  default = "Z0057140DV55FUG37SFU"
+}
+
+variable "domain_name" {
+  default = "bittu27.site"
+}
+
+variable "common_tags"{
+  default = {
+    project = "roboshop"
+    Terraform = true
+  }
+}
